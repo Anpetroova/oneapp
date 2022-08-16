3055e95c8fa90b234726c4035eb55d86b6303796
Anpetroova <ageenko@rt-tech.ru>
2022-08-16 23:00:50 +0300

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -9,20 +10,20 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Последний фильм?', ''),
-      b = prompt('На сколько оцениту его?',''),
-      c = prompt('Последний фильм?', ''),
-      d = prompt('На сколько оцениту его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Последний фильм?', ''),
+          b = prompt('На сколько оцениту его?','');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
-
-
-
-
-
-
-
-
